a5ce50865941a0b91375417f5795eaa781ba39e9
cme2me <max.zhakov@gmail.com>
2022-02-09 19:32:28 +0300

    First task done

--- src/Lesson3/Main.java
+++ src/Lesson3/Main.java
@@ -1,5 +1,18 @@
 package Lesson3;
 
-public class Main {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+public class Main {
+    public static void main(String[] args) {
+        String[] words= {"Hello", "Hello", "Kick", "Jump", "Fly", "Kick", "Run", "Depend", "Spend", "Love","Roll"};
+        System.out.println(Arrays.toString(words));
+
+        Map<String, Integer> hashMap = new HashMap<>();
+        for (String n : words) {
+            hashMap.put(n, hashMap.getOrDefault(n, 0)+1);
+        }
+        System.out.println(hashMap);
+    }
 }
