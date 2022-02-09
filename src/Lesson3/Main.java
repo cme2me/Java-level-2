1c1eb395b529a4c6a2a8f0fb1e0ede139efefc64
cme2me <max.zhakov@gmail.com>
2022-02-09 19:47:34 +0300

    Homework is done

--- src/Lesson3/Main.java
+++ src/Lesson3/Main.java
@@ -14,5 +14,23 @@ public class Main {
             hashMap.put(n, hashMap.getOrDefault(n, 0)+1);
         }
         System.out.println(hashMap);
+
+        PhoneNumbers phNum = new PhoneNumbers();
+        phNum.add("Насонов", "89554442021");
+        phNum.add("Семченко", "8955331233");
+        phNum.add("Насонов", "89551212312");
+        phNum.add("Топлутов", "890235943");
+        phNum.add("Сварщиков", "854664323");
+        phNum.add("Водоёмов", "89685465432");
+        phNum.add("Уткина", "89998886655");
+        phNum.add("Человекина", "89654923920");
+
+        System.out.println("Телефон: " + phNum.get("Насонов"));
+        System.out.println("Телефон: " + phNum.get("Водоёмов"));
+        System.out.println("Телефон: " + phNum.get("Уткина"));
+        System.out.println("Телефон: " + phNum.get("Топлутов"));
+        System.out.println("Телефон: " + phNum.get("Семченко"));
+        System.out.println("Телефон: " + phNum.get("Сварщиков"));
+        System.out.println("Телефон: " + phNum.get("Человекина"));
     }
 }
--- src/Lesson3/PhoneNumbers.java
+++ src/Lesson3/PhoneNumbers.java
@@ -1,4 +1,19 @@
 package Lesson3;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class PhoneNumbers {
+    private Map<String, Set<String>> phoneList = new HashMap<>();
+
+    public void add(String name, String number){
+        Set<String> tell = phoneList.getOrDefault(name, new HashSet<>());
+        tell.add(number);
+        phoneList.put(name, tell);
+    }
+    public Set<String> get(String name){
+        return phoneList.get(name);
+    }
 }
